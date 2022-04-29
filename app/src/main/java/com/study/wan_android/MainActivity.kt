592aa2149bd79dbbc4f3e6507f3fbfd632493698
jiangker <niklaus1996@icloud.com>
2022-04-29 22:02:53 +0800

    优化路由跳转

--- app/src/main/java/com/study/wan_android/MainActivity.kt
+++ app/src/main/java/com/study/wan_android/MainActivity.kt
@@ -5,16 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.study.wan_android.ui.page.main.MainPage
 import com.study.wan_android.ui.theme.WanandroidTheme
@@ -23,11 +27,16 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    val routes = listOf(
+        Route.Main,
+        Route.Classify,
+        Route.Mine
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val navController = rememberNavController()
-            val selectIndex by remember { mutableStateOf(0) }
             WanandroidTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -36,17 +45,28 @@ class MainActivity : ComponentActivity() {
                     Scaffold(
                         bottomBar = {
                             BottomNavigation {
-                                Route.navigationList.mapIndexed { index, s ->
+                                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                                val currentDestination = navBackStackEntry?.destination
+                                routes.forEach { route ->
                                     BottomNavigationItem(
-                                        selected = selectIndex == index,
-                                        onClick = { navController.navigate(s) },
+                                        selected = currentDestination?.hierarchy?.any { it.route == route.route } == true,
+                                        onClick = {
+                                            navController.navigate(route.route) {
+                                                popUpTo(navController.graph.findStartDestination().id) {
+                                                    saveState = true
+                                                }
+                                                launchSingleTop = true
+                                                restoreState = true
+                                            }
+                                        },
                                         label = {
-                                            Text(text = s)
+                                            Text(text = stringResource(route.resourceId))
                                         },
                                         icon = {
                                             Icon(
-                                                bitmap = ImageBitmap(width = 50, height = 50),
-                                                contentDescription = ""
+                                                painter = painterResource(id = route.drawableId),
+                                                contentDescription = "",
+                                                modifier = Modifier.size(25.dp)
                                             )
                                         },
                                     )
@@ -56,14 +76,17 @@ class MainActivity : ComponentActivity() {
                     ) {
                         NavHost(
                             navController = navController,
-                            startDestination = Route.MAIN,
+                            startDestination = Route.Main.route,
                             modifier = Modifier.padding(bottom = it.calculateBottomPadding())
                         ) {
-                            composable(Route.MAIN) {
+                            composable(Route.Main.route) {
                                 MainPage()
                             }
-                            composable(Route.MINE) {
-                                Text(text = Route.MINE)
+                            composable(Route.Classify.route) {
+                                Text(text = Route.Classify.route)
+                            }
+                            composable(Route.Mine.route) {
+                                Text(text = Route.Mine.route)
                             }
                         }
                     }
--- app/src/main/java/com/study/wan_android/Route.kt
+++ app/src/main/java/com/study/wan_android/Route.kt
@@ -1,10 +1,16 @@
 package com.study.wan_android
 
-object Route {
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 
-    const val MAIN: String = "main"
-    const val MINE: String = "mine"
+sealed class Route(
+    val route: String,
+    @StringRes val resourceId: Int,
+    @DrawableRes val drawableId: Int
+) {
 
+    object Main : Route("main", R.string.tab_main, R.drawable.ic_mian)
+    object Classify : Route("classify", R.string.tab_classify, R.drawable.ic_classify)
+    object Mine : Route("mine", R.string.tab_mine, R.drawable.ic_mine)
 
-    val navigationList = listOf(MAIN, MINE)
 }
