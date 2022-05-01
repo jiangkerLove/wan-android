d7180bf71c66dea04cf65385350febdb8c42faf0
jiangker <niklaus1996@icloud.com>
2022-05-01 13:32:28 +0800

    使用FlowRow

--- app/src/main/java/com/study/wan_android/ui/page/classify/SystemPage.kt
+++ app/src/main/java/com/study/wan_android/ui/page/classify/SystemPage.kt
@@ -1,9 +1,9 @@
 package com.study.wan_android.ui.page.classify
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -16,7 +16,9 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.accompanist.flowlayout.FlowRow
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun SystemPage(
     systemModel: SystemModel = hiltViewModel()
@@ -30,15 +32,15 @@ fun SystemPage(
             .background(color = Color(0xFFF5F5F5))
     ) {
         LazyColumn {
-            items(groupList.value.size) { index ->
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 4.dp)
-                        .background(color = Color.White)
-                        .padding(vertical = 12.dp, horizontal = 12.dp)
-                ) {
-                    Row(verticalAlignment = Alignment.CenterVertically) {
+            groupList.value.forEach { system ->
+                stickyHeader {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(color = Color.White)
+                            .padding(start = 12.dp)
+                    ) {
                         Box(
                             modifier = Modifier
                                 .size(width = 8.dp, height = 16.dp)
@@ -47,23 +49,31 @@ fun SystemPage(
                         )
                         Box(modifier = Modifier.size(width = 4.dp, height = 0.dp))
                         Text(
-                            text = groupList.value[index].name,
+                            text = system.name,
                             fontWeight = FontWeight.W700,
                             fontSize = 16.sp
                         )
                     }
-                    Box(modifier = Modifier.size(12.dp))
-                    Row {
-                        groupList.value[index].childes.forEach {
-                            Text(
-                                text = it.name,
-                                modifier = Modifier
-                                    .padding(end = 4.dp)
-                                    .clip(shape = RoundedCornerShape(8.dp))
-                                    .background(color = Color(0XFFF1F6FD))
-                                    .padding(4.dp),
-                                color = Color(0xFF6FA1F1)
-                            )
+                }
+                item {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 4.dp)
+                            .background(color = Color.White)
+                            .padding(start = 12.dp, end = 12.dp, top = 12.dp, bottom = 16.dp)
+                    ) {
+                        FlowRow(mainAxisSpacing = 4.dp, crossAxisSpacing = 4.dp) {
+                            system.childes.forEach {
+                                Text(
+                                    text = it.name,
+                                    modifier = Modifier
+                                        .clip(shape = RoundedCornerShape(8.dp))
+                                        .background(color = Color(0XFFF1F6FD))
+                                        .padding(4.dp),
+                                    color = Color(0xFF6FA1F1)
+                                )
+                            }
                         }
                     }
                 }
