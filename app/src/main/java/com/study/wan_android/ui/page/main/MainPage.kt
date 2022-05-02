fde8b374cbffbffb38c4be3d7fccb0c1254fb705
jiangker <niklaus1996@icloud.com>
2022-05-02 15:42:18 +0800

    增加viewpager，但存在和lazy列表切换奔溃的问题

--- app/src/main/java/com/study/wan_android/ui/page/main/MainPage.kt
+++ app/src/main/java/com/study/wan_android/ui/page/main/MainPage.kt
@@ -1,67 +1,72 @@
 package com.study.wan_android.ui.page.main
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.paging.compose.collectAsLazyPagingItems
-import com.study.wan_android.data.model.ArticleModel
-import androidx.paging.compose.items
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
+import com.study.wan_android.ui.page.main.content.RecommendContent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun MainPage(
     viewModel: MainPageModel = hiltViewModel(),
 ) {
-    val pagingItems = viewModel.articleList.collectAsLazyPagingItems()
-    LazyColumn(modifier = Modifier.background(color = Color(0x455F5F5F))) {
-        items(pagingItems) { article ->
-            ArticleCompose(article = article!!)
+
+    val pagerState = rememberPagerState()
+    val state: LazyListState = rememberLazyListState()
+
+    Scaffold(topBar = {
+        TabRow(
+            selectedTabIndex = pagerState.currentPage,
+            indicator = { tabPositions ->
+                TabRowDefaults.Indicator(
+                    Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
+                )
+            }
+        ) {
+            MainTab.tabs.forEachIndexed { index, tab ->
+                Tab(
+                    text = { Text(tab.tabName) },
+                    selected = pagerState.currentPage == index,
+                    onClick = {
+                        CoroutineScope(Dispatchers.Main).launch {
+                            pagerState.scrollToPage(index, pageOffset = 0F)
+                        }
+                    },
+                )
+            }
         }
     }
-
-}
-
-
-@Composable
-fun ArticleCompose(article: ArticleModel) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 5.dp, horizontal = 10.dp)
-            .clip(shape = RoundedCornerShape(10.dp))
-            .background(color = Color.White)
     ) {
-        Text(
-            text = article.title,
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(color = Color.Red)
-                .padding(5.dp),
-        )
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(5.dp)
-        ) {
-            if (article.author.isNotEmpty()) {
-                Text(text = "作者：${article.author}", modifier = Modifier.padding(end = 5.dp))
+        HorizontalPager(
+            count = MainTab.tabs.size,
+            state = pagerState,
+            contentPadding = PaddingValues(top = it.calculateTopPadding())
+        ) { index ->
+            when (MainTab.tabs[index]) {
+                MainTab.Recommend -> {
+                    RecommendContent(viewModel, state)
+                }
+                MainTab.Plaza -> {
+                    Text(text = "Plaza")
+                }
+                MainTab.Reply -> {
+                    Text(text = "Reply")
+                }
+                MainTab.Term -> {
+                    Text(text = "Term")
+                }
             }
-            if (article.shareUser.isNotEmpty()) {
-                Text(text = "分享人：${article.shareUser}", modifier = Modifier.padding(end = 5.dp))
-            }
-            Text(
-                text = "分类：${article.superChapterName}",
-                modifier = Modifier.padding(end = 5.dp)
-            )
-            Text(text = article.formatTime)
         }
     }
 }
